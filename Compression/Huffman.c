8928d41f0cdcc0249f1511609f3d7c7219f0f280
Lucas de Souza Vieira <lucaspakfa@gmail.com>
2016-03-21 00:36:32 -0300

    Revert "OhShit"
    This reverts commit 794138fc177c698b77adbb251378f0e1da9f28eb.

---
+++ Compression/Huffman.c
@@ -0,0 +1,30 @@
+/**
+Este arquivo é parte do projeto Data Structures
+Este é um software livre; você pode redistribuí-lo e/ou
+modificá-lo dentro dos termos da Licença Pública Geral GNU como
+publicada pela Fundação do Software Livre (FSF); na versão 3 da
+Licença, ou (na sua opinião) qualquer versão.
+
+Este programa é distribuído na esperança de que possa ser  útil,
+mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+Licença Pública Geral GNU para maiores detalhes.
+
+Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+
+Arquivo: Huffman.c
+Descrição: Implementação de funções para codificação de Huffman
+Autor: Lucas de Souza Vieira <lukaslka_my08@hotmail.com>	*/
+
+#include "Huffman.h"
+
+string * encodeHuffman(string * myString)
+{
+	return NULL;
+}
+
+string * decodeHuffman(string * myString)
+{
+	return NULL;
+}
